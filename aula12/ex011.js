6d989bca7f3af57ffeb5f60b69f4c6441fab0009
Wesley Medeiros <wescarmed@outlook.com>
2024-09-18 19:07:23 -0300

    aula 12 encerrada

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 28
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade >=18 && idade < 60) {
+    console.log('Voto obrigatório')
+} else {
+    console.log('Voto facultativo')
+}
---
+++ aula12/prática002.js
@@ -0,0 +1,12 @@
+// Praticando o uso do Switch
+
+var num = 7
+var res = num % 2
+
+switch (res) {
+    case 0:
+        console.log('Par')
+        break
+    case 1:
+        console.log('Ímpar')
+}
